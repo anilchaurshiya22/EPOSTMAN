216f7910cd5c78a7da422afeae55ae517073e12f
deep risal <keithaniz@gmail.com>
2015-04-07 03:51:38 +0000

    populate.sql edited online with Bitbucket

--- src/main/resources/sql/populate.sql
+++ src/main/resources/sql/populate.sql
@@ -9,4 +9,7 @@ INSERT INTO `USER` (`DESCRIPTION`, `EMAIL`, `FIRSTNAME`, `LASTLOGINDATE`, `LASTN
 (NULL, 'admin@gmail.com', 'admin', NULL, 'admin', 'admin123', 1, '4252563461', 'M', 0, 'admin', '/resource/images/4.png', 2),
 (NULL, 'user@gmail.com', 'user', NULL, 'user', 'user123', 1, '5437457123', 'F', 1, 'user', NULL, 3);
 
+INSERT INTO `mail_item` (`ID`, `ARRIVALDATE`, `BARCODE`, `DEPARTUREDATE`, `DESCRIPTION`, `TYPE`, `NAME`, `STATUS`, `USERID`) VALUES
+(2, '2014-12-13 00:00:00', '12334345654747877qw', '2014-12-14 00:00:00', 'Jeans Jacket and Tshirt and Pant at affordable price', 0, 'Jeans Jacket and Tshirt and Pant', 1, 1);
+
 
